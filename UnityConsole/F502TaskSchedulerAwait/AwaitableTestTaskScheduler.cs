756e4354f907d86287b387ed1ef6546387d1987c
aminov <alami@tshtt.uz>
2022-08-25 09:20:36 +0500

    ch5-02 TaskSchedulerAwait

---
+++ UnityConsole/F502TaskSchedulerAwait/AwaitableTestTaskScheduler.cs
@@ -0,0 +1,31 @@
+﻿namespace F50TaskSchedulerAwait
+{
+    internal class AwaitableTestTaskScheduler : TaskScheduler
+    {
+        private int counter = 0;
+        string[] names = { "1ЙПОТОК", "2ЙПОТОК", "3ЙПОТОК", "4ЙПОТОК" };
+        protected override void QueueTask(Task task)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"QueueTask сработал для задачи - {task.Id}");
+            Console.ResetColor();
+            new Thread(_=> base.TryExecuteTask(task)) 
+                    { IsBackground=true, Name = names[counter++] }
+                .Start();
+            //ThreadPool.QueueUserWorkItem(_=> base.TryExecuteTask(task));
+        }
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"TryExecuteTaskInline сработал для задачи - {task.Id}");
+            Console.ResetColor();
+            return base.TryExecuteTask(task);
+        }
+        protected override IEnumerable<Task>? GetScheduledTasks()
+        {
+            return null;
+        }
+    }
+}
---
+++ UnityConsole/F502TaskSchedulerAwait/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F50TaskSchedulerAwait
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.SetWindowSize(90,40);
+            ShowData("Main выполнился до await");
+            //var mainTask = new Task<Task>(MethodAsync);
+            var mainTask = new Task<Task>(MethodAsync);
+            mainTask.Start(new AwaitableTestTaskScheduler());
+            await await mainTask;
+            //var result = await mainTask;
+            //await result;
+            ShowData("Main выполнился после await");
+            Console.ReadKey();
+        }
+        private static async Task MethodAsync()
+        {
+            ShowData("MethodAsync выполнился до await");
+            var task = new Task(TestMethod, TaskCreationOptions.HideScheduler);
+            task.Start();
+            await task;
+            ShowData("MethodAsync выполнился после await");
+        }
+        private static void TestMethod()
+        {
+            ShowData("TestMethod выполнился");
+        }
+        private static void ShowData(string v)
+        {
+            Console.WriteLine($"Имя потока: {Thread.CurrentThread.Name}");
+            Console.WriteLine($"Id потока: {Thread.CurrentThread.ManagedThreadId}." +
+                $" Поток из пула потоков: {Thread.CurrentThread.IsThreadPoolThread}");
+            Console.WriteLine($"Id Задачи: {Task.CurrentId}");
+            Console.WriteLine($"Текущий планировщик задач: " +
+$"{typeof(TaskScheduler).GetProperty("InternalCurrent", BindingFlags.Static| BindingFlags.NonPublic).GetValue(typeof(TaskScheduler))}");
+            Console.WriteLine(new string('-', 80));
+            Console.WriteLine();
+        }
+    }
+}
