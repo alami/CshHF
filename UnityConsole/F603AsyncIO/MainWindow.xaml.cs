0eb377483e2328aab6f4d9505ea52a88db4d2bc3
alami <alami@km.ru>
2022-08-28 19:09:20 +0500

    ch6-03,4 Async IO

---
+++ UnityConsole/F603AsyncIO/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace F603AsyncIO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            File.Create("text.txt").Dispose();
+            MonitorIOCP();
+        }
+
+
+        private async void BtnRead_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("text.txt", FileMode.Open,
+                FileAccess.Read, FileShare.ReadWrite, 4096, FileOptions.Asynchronous))
+            {
+                //liRead.Visibility = Visibility.Visible;
+
+                byte[] bytes = new byte[fs.Length];
+                await fs.ReadAsync(bytes, 0, bytes.Length);
+                string fileContent = Encoding.UTF8.GetString(bytes);
+
+                //liRead.Visibility = Visibility.Hidden;
+                txtOutput.Text = fileContent;
+            }
+        }
+
+        private async void BtnWrite_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("text.txt", FileMode.Open,
+                FileAccess.Write, FileShare.ReadWrite, 4096, FileOptions.Asynchronous))
+            {
+                //liWrite.Visibility = Visibility.Visible;
+
+                string textBoxContent = txtOutput.Text;
+                byte[] bytes = Encoding.UTF8.GetBytes(textBoxContent);
+                await fs.WriteAsync(bytes, 0, bytes.Length);
+
+                //liWrite.Visibility = Visibility.Hidden;
+                txtOutput.Clear();
+            }
+        }
+        private void MonitorIOCP()
+        {
+            new Thread(() => {
+                while (true)
+                {
+                    ThreadPool.GetAvailableThreads(out int threads, out int IOCP);
+                    ThreadPool.GetMaxThreads(out int maxThreads, out int maxIOCP);
+                    if (IOCP < maxIOCP)
+                    {
+                        Dispatcher.Invoke(() => {
+                            txtIOCP.Text += $"IOCP[{IOCP}  из {maxIOCP}]{Environment.NewLine}";
+                        });
+                    }
+                }
+            })
+            { IsBackground = true }.Start();
+        }
+    }
+}
---
+++ UnityConsole/F604AsyncIO/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace F604AsyncIO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HttpClient httpClient = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MonitorIOCP();
+        }
+
+        private async void BtnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            //li.Visibility = Visibility.Visible;
+            string httpContent = await httpClient.GetStringAsync("http://microsoft.com");
+            //li.Visibility = Visibility.Hidden;
+            txtOutput.Text = httpContent;
+
+        }
+
+        private void MonitorIOCP()
+        {
+            new Thread(() => {
+                while (true)
+                {
+                    ThreadPool.GetAvailableThreads(out int threads, out int IOCP);
+                    ThreadPool.GetMaxThreads(out int maxThreads, out int maxIOCP);
+                    if (IOCP < maxIOCP)
+                    {
+                        Dispatcher.Invoke(() => {
+                            txtIOCP.Text += $"IOCP[{IOCP}  из {maxIOCP}]{Environment.NewLine}";
+                        });
+                    }
+                }
+            })
+            { IsBackground = true }.Start();
+        }
+    }
+}
