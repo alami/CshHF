0b6c72c2486abd56b8c0ca1b358f957d00c2b6cc
alami <alami@km.ru>
2022-09-03 17:29:47 +0500

    ch7-51 Progress

---
+++ UnityConsole/F751Report/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace F751Report
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static private string nL = $"{Environment.NewLine}";
+        static private string nLnL = $"{Environment.NewLine}--------------------{Environment.NewLine}";
+        private IProgress<int> progress;
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.progress = new Progress<int>(ProgressBarUpdate);
+        }
+
+        private void ProgressBarUpdate(int value)
+        {
+            this.pb.Value = value;
+        }
+
+        private async void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            pb.Value = 0;
+            btnStart.IsEnabled = false;
+
+            Operation operation = new Operation();
+            try
+            {
+                var nums = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                int sum = await operation.SumNumbersAsync(nums, this.cts.Token, this.progress);
+                txtRes.Text += $"Операция завершена. Сумма чисел ({GetNumbers(nums)}) = {sum}{nLnL}";
+            } catch (OperationCanceledException ex)
+            {
+                this.cts = new CancellationTokenSource();
+                txtRes.Text += $"Операция отменена.{nLnL}";
+            }
+            catch (Exception ex)
+            {
+                string exceptionBody = $"Ошибка: {ex.GetType()}{nL}{ex.Message}";
+                txtRes.Text += $"{exceptionBody}{nLnL}";
+            }
+            btnStart.IsEnabled = true;
+        }
+
+        private object GetNumbers(int[] numbers)
+        {
+            StringBuilder sb=new StringBuilder();
+            foreach (int num in numbers)
+            {
+                sb.Append(num).Append(' ');
+            }
+            return sb.ToString().TrimEnd(' ');
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+        }
+    }
+}
---
+++ UnityConsole/F751Report/Operation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace F751Report
+{
+    internal class Operation
+    {
+        internal Task<int> SumNumbersAsync(int[] numbers, CancellationToken token = default,
+            IProgress<int> progress=null)
+        {
+            return Task.Run(() => SumNumbers(numbers, token, progress), token);
+        }
+
+        private int SumNumbers(int[] numbers, CancellationToken token, IProgress<int> progress)
+        {
+            token.ThrowIfCancellationRequested();
+            int sum = 0;
+            for (int i = 0,j=1; i < numbers.Length; i++,j++)
+            {
+                Thread.Sleep(1000);
+                sum += numbers[i];
+                token.ThrowIfCancellationRequested();
+                progress?.Report((j*100)/numbers.Length);
+            }
+            return sum;
+        }
+    }
+}
